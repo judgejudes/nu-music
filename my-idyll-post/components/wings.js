6c29465f710a0398e3109af6e6cf3d0c460b311c
judgejudes <judylee2021@u.northwestern.edu>
2021-05-26 23:14:50 -0400

    added svg

--- my-idyll-post/components/wings.js
+++ my-idyll-post/components/wings.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import * as d3 from "d3"
+import * as d3 from "d3";
+import albums from '../data/decades-albums';
 
 class Wings extends React.PureComponent {
     constructor(props) {
         super(props);
     }
 
+    test() {
+        console.log('whats going on')
+    }
+
     render() {
         // ... rendered output here
+
         return (
-            <svg>
-                <circle cx="0" cy="0" r="10"></circle>
-            </svg>
+            // albums.forEach((album) => {
+
+            // <svg viewBox="0 0 210 297" width="100%" height="400" className="petal">
+            //     <path fill='none' stroke='#000' d='M0,0 C50,40 50,70 20,100 L0,85 L-20,100 C-50,70 -50,40 0,0' />
+            // </svg>
+            // }
+
+            albums.map(album => {
+                return (
+                    <svg viewBox="0 0 100 297" className="petal">
+                        <path fill="none" stroke='#000' d='M0,0 C50,40 50,70 20,100 L0,85 L-20,100 C-50,70 -50,40 0,0' />
+                    </svg>
+
+// fill=${album. 
+                )
+            })
         )
     }
 }
---
+++ my-idyll-post/data/decades-albums.js
@@ -0,0 +1,20 @@
+const albums = [
+  {
+    "decade": "1960",
+    "title": "At Last",
+    "artist": "Etta James"
+
+  },
+  {
+    "decade": "1960",
+    "title": "Revolver",
+    "artist": "The Beatles"
+  },
+  {
+    "decade": "1960",
+    "title": "The White Album",
+    "artist": "The Beatles"
+  },
+];
+
+module.exports = albums;
